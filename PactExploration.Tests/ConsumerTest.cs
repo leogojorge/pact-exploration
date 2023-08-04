adcf0278e2233a9ec40df6b226f179ded005c629
leogojorge <leogojorge@outlook.com>
2023-08-04 19:16:08 -0300

    add tow more test cases

--- PactExploration.Tests/ConsumerTest.cs
+++ PactExploration.Tests/ConsumerTest.cs
@@ -33,18 +33,62 @@ namespace TestProject1
         }
 
         [Fact]
-        public async void ValidGetOnPostmanApi()
+        public async void ValidGetOnProviderApi()
         {
             // Arange
             var expectedResponse = GeneretaGetSomeDataResponse();
 
             pact.UponReceiving("A get an entity from a provider")
-                .Given("It already has 2 entities") //essa string se transforma no ProviderState quando o teste chega no lado do provedor. ele é usado pra poder testar um endpoint com cenários diferentes. ex: get usuário GIVEN usuário inexistente.
+                .Given("2 initial products created") //essa string se transforma no ProviderState quando o teste chega no lado do provedor. ele é usado pra poder testar um endpoint com cenários diferentes. ex: get usuário GIVEN usuário inexistente.
                 .WithRequest(HttpMethod.Get, "/api/some-data")
             .WillRespond()
                 .WithStatus(HttpStatusCode.OK)
                 .WithHeader("Content-Type", "application/json; charset=utf-8")
                 .WithJsonBody(new TypeMatcher(expectedResponse));
+
+            //act assert
+            await pact.VerifyAsync(async context =>
+            {
+                this.WeConsumingSomeone = new WeConsumingSomeone(context.MockServerUri); //mermão, se a url da api for diferente da url q o Pact usa,o teste quebra, pois o Pact entende que são serviços diferentes, então ele considera que o serviço que ele está olhando não recebeu nenhuma requisição
+                var response = await WeConsumingSomeone.GetSomeData();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            });
+        }
+
+        public async void Validate_GetProductById_WithExistingId_OnProviderApi()
+        {
+            // Arange
+            var expectedResponse = GeneretaGetSomeDataResponse();
+
+            pact.UponReceiving("A get entity by id from a provider")
+                .Given("Products with id 993a1ad5-7f7a-4a91-91fb-c0ee62755a2d exist")
+                .WithRequest(HttpMethod.Get, "/api/some-data/")
+            .WillRespond()
+                .WithStatus(HttpStatusCode.OK)
+                .WithHeader("Content-Type", "application/json; charset=utf-8")
+                .WithJsonBody(new TypeMatcher(null));
+
+            //act assert
+            await pact.VerifyAsync(async context =>
+            {
+                this.WeConsumingSomeone = new WeConsumingSomeone(context.MockServerUri); //mermão, se a url da api for diferente da url q o Pact usa,o teste quebra, pois o Pact entende que são serviços diferentes, então ele considera que o serviço que ele está olhando não recebeu nenhuma requisição
+                var response = await WeConsumingSomeone.GetSomeData();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            });
+        }
+
+        public async void Validate_GetProductById_WithNonExistantId_OnProviderApi()
+        {
+            // Arange
+            var expectedResponse = GeneretaGetSomeDataResponse();
+
+            pact.UponReceiving("A get entity by id from a provider")
+                .Given("Id not stored")
+                .WithRequest(HttpMethod.Get, "/api/some-data/")
+            .WillRespond()
+                .WithStatus(HttpStatusCode.OK)
+                .WithHeader("Content-Type", "application/json; charset=utf-8")
+                .WithJsonBody(new TypeMatcher(null));
 
             //act assert
             await pact.VerifyAsync(async context =>
